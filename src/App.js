89aa997b77cb0ee0740f9e2d78edc54e524e200b
yash01rai <yash.rai603@gmail.com>
2021-02-18 21:50:16 +0530

    project 3

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Review from "./Review";
+function App() {
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>Our reviews</h2>
+        </div>
+        <Review />
+      </section>
+    </main>
+  );
+}
+
+export default App;
